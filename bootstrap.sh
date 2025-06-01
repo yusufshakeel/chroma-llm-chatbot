805ad25e3fb61447543bed2b4e55a84e2cd213e3
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-06-01 13:37:35 +0530

    Add bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Create documents folder and create a README.txt file inside it.
+mkdir -p documents
+echo "Put your files inside this folder. \
+It will be used to answer your queries." > ./documents/README.txt
