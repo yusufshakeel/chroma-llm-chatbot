25ed511a974379cf1449b4346dfb2c98a20a8d02
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-06-06 14:22:39 +0530

    Refactoring work, added a dummy company details

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,11 +1,8 @@
 import chromadb
 from chromadb.utils import embedding_functions
-from env_config import CHROMA_DATABASE_PATH, COLLECTION_NAME, OFFLINE_MODE_ENABLED
+from env_config import CHROMA_DATABASE_PATH, COLLECTION_NAME
 
-if OFFLINE_MODE_ENABLED:
-    emb_fn = embedding_functions.DefaultEmbeddingFunction()
-else:
-    emb_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+emb_fn = embedding_functions.DefaultEmbeddingFunction()
 
 client = chromadb.PersistentClient(path=CHROMA_DATABASE_PATH)
 
